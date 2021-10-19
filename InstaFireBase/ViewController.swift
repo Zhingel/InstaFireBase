eaf1ea8e69579ce8f3f0cd4288a4684aea633e0a
Stas <staszhingel@icloud.com>
2021-10-19 20:02:06 +0300

    Initial Commit

---
+++ InstaFireBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaFireBase
+//
+//  Created by Стас Жингель on 19.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
