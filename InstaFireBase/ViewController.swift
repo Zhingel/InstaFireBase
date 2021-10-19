79387c7cba55980303cadcda3602bb69ca22adff
Stas <staszhingel@icloud.com>
2021-10-19 20:19:42 +0300

    Login view

--- InstaFireBase/ViewController.swift
+++ InstaFireBase/ViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let plusPhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "plus_photo"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(plusPhotoButton)
+        plusPhotoButton.heightAnchor.constraint(equalToConstant: 140).isActive = true
+        plusPhotoButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
+        plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        plusPhotoButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
     }
 
 
