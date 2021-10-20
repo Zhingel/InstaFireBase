1da4687b3511ac064a785a7d954c1568a0aa4982
Stas <staszhingel@icloud.com>
2021-10-20 12:26:57 +0300

    extension for textfields

--- InstaFireBase/ViewController.swift
+++ InstaFireBase/ViewController.swift
@@ -11,8 +11,25 @@ class ViewController: UIViewController {
     
     let plusPhotoButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setImage(UIImage(named: "plus_photo"), for: .normal)
+        button.setImage(UIImage(named: "plus_photo")?.withRenderingMode(.alwaysOriginal), for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    let emailTextfield = UITextField(placeholder: "Email")
+    let userNameTextfield = UITextField(placeholder: "Username")
+    let passwordTextfield: UITextField = {
+        let tf = UITextField(placeholder: "Password")
+        tf.isSecureTextEntry = true
+        return tf
+    }()
+    
+    let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = UIColor(red: 149/255, green: 204/255, blue: 244/255, alpha: 1)
+        button.setTitle("Sign Up", for: .normal)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font =  UIFont.systemFont(ofSize: 14)
+        button.setTitleColor(.white, for: .normal)
         return button
     }()
 
@@ -23,8 +40,26 @@ class ViewController: UIViewController {
         plusPhotoButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
         plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         plusPhotoButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        
+        setupSignUpView()
+       
     }
 
-
+    fileprivate func setupSignUpView() {
+        
+        let stackView = UIStackView(arrangedSubviews: [emailTextfield, userNameTextfield, passwordTextfield, signUpButton])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: plusPhotoButton.bottomAnchor, constant: 40),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            stackView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+    }
 }
 
