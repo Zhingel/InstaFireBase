4f60f4588fd9aa9bc22104c77a82131db4efb2dd
Stas <staszhingel@icloud.com>
2021-10-24 13:59:19 +0300

    fetch data and images from firebase

--- InstaFireBase/UserProfile/UserProfileController.swift
+++ InstaFireBase/UserProfile/UserProfileController.swift
@@ -11,10 +11,11 @@ import FirebaseAuth
 import FirebaseDatabase
 
 class UserProfileController : UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    var user: User?
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
-        collectionView.register(UICollectionViewCell.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader , withReuseIdentifier: "headerID")
+        collectionView.register(UserProfileHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader , withReuseIdentifier: "headerID")
         collectionView.backgroundColor = .white
         fetchUsers()
     }
@@ -23,8 +24,9 @@ class UserProfileController : UICollectionViewController, UICollectionViewDelega
         guard let uid = Auth.auth().currentUser?.uid else {return}
         Database.database().reference().child("users").child(uid).observeSingleEvent(of: .value, with: { snapshot in
             guard let dictionary = snapshot.value as? [String: Any] else {return}
-            let username = dictionary["username"] as? String
-            self.navigationItem.title = username
+            self.user = User(dictionary: dictionary)
+            self.navigationItem.title = self.user?.username
+            self.collectionView.reloadData()
         }) {err in
             print("fail fetch user", err)
         }
@@ -33,8 +35,8 @@ class UserProfileController : UICollectionViewController, UICollectionViewDelega
     
     
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "headerID", for: indexPath)
-        header.backgroundColor = .green
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "headerID", for: indexPath) as! UserProfileHeader
+        header.user = self.user
         return header
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
@@ -48,5 +50,15 @@ class UserProfileController : UICollectionViewController, UICollectionViewDelega
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
         cell.backgroundColor = .red
         return cell
+    }
+}
+
+
+struct User {
+    let username: String
+    let profileImageURL: String
+    init(dictionary: [String: Any]) {
+        self.username = dictionary["username"] as? String ?? ""
+        self.profileImageURL = dictionary["profileImageUrl"] as? String ?? ""
     }
 }
--- InstaFireBase/UserProfile/UserProfileHeader.swift
+++ InstaFireBase/UserProfile/UserProfileHeader.swift
@@ -8,5 +8,41 @@
 import UIKit
 
 class UserProfileHeader: UICollectionViewCell {
+    let profileImage = UIImageView()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .blue
+        addSubview(profileImage)
+        profileImage.constraints(top: self.topAnchor, bottom: nil, left: self.leftAnchor, right: nil, paddingTop: 20, paddingBottom: 0, paddingLeft: 10, paddingRight: 0, width: 80, height: 80)
+        profileImage.backgroundColor = .red
+        profileImage.layer.cornerRadius = 80/2
+        profileImage.clipsToBounds = true
+        
+        
+    }
+    
+    var user: User? {
+        didSet {
+            guard let profileImageURL = user?.profileImageURL else {return}
+            guard let url = URL(string: profileImageURL) else {return}
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if let error = error {
+                    print("Failed to fetch profile Image:", error)
+                    return
+                }
+                guard let data = data else {return}
+                let image = UIImage(data: data)
+                DispatchQueue.main.async {
+                    self.profileImage.image = image
+                }
+            }.resume()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
     
 }
