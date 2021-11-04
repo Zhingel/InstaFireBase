b2716c456990fcbfaa9b770feebc8a8657dbf551
Stas <staszhingel@icloud.com>
2021-11-04 11:28:05 +0300

    saved image to library

--- InstaFireBase/PreviewPhotoContainer.swift
+++ InstaFireBase/PreviewPhotoContainer.swift
@@ -28,13 +28,34 @@ class PreviewPhotoContainer: UIViewController {
         iv.clipsToBounds = true
         return iv
     }()
+    let saveButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "save_shadow"), for: .normal)
+        button.addTarget(self, action: #selector(handleSave), for: .touchUpInside)
+        return button
+    }()
+    @objc fileprivate func handleSave() {
+        guard let images = selectedImage else {return}
+            UIImageWriteToSavedPhotosAlbum(images, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            // we got back an error!
+            print(error)
+           
+        } else {
+            print("Sucess")
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
         view.addSubview(imageView)
         view.addSubview(cancelButton)
         view.addSubview(dismissButton)
+        view.addSubview(saveButton)
         let height = (view.bounds.height - view.bounds.width)/2
+        saveButton.constraints(top: nil, bottom: view.bottomAnchor, left: view.leftAnchor, right: nil, paddingTop: 0, paddingBottom: -12, paddingLeft: 12, paddingRight: 0, width: 50, height: 50)
         cancelButton.constraints(top: view.topAnchor, bottom: nil, left: view.leftAnchor, right: nil, paddingTop: 12, paddingBottom: 0, paddingLeft: 12, paddingRight: 0, width: 50, height: 50)
         imageView.constraints(top: view.topAnchor, bottom: view.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: height, paddingBottom: -height, paddingLeft: 0, paddingRight: 0, width: view.bounds.width, height: view.bounds.width)
         dismissButton.constraints(top: view.topAnchor, bottom: nil, left: nil, right: view.rightAnchor, paddingTop: 12, paddingBottom: 0, paddingLeft: 0, paddingRight: 12, width: 50, height: 50)
